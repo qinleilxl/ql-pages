c98a920d3f6c6268a8012d00bd594181edd3e092
qinlei <qinleilxl@outlook.com>
2020-10-23 19:04:41 +0800

    feat:update package

--- bin/ql-pages.js
+++ bin/ql-pages.js
@@ -3,8 +3,6 @@
 process.argv.push('--cwd') // 指定新的当前工作目录
 process.argv.push(process.cwd())
 process.argv.push('--gulpfile')
-// process.argv.push(require.resolve('../lib/index.js'))
 process.argv.push(require.resolve('..'))
 
-console.log(process.argv);
 require('gulp/bin/gulp');
